e0e2cbc9a89678a4f7d96be1c075dcf702a32ded
ShikiIsDev <104403831+ShikiIsDev@users.noreply.github.com>
2024-11-15 19:03:34 +0800

    commit msg

--- src/lib/supabaseClient.js
+++ src/lib/supabaseClient.js
@@ -1,8 +1,5 @@
-import { createClient } from '@supabase/supabase-js';
-import { VITE_SUPABASE_URL, VITE_SUPABASE_ANON_KEY } from "$env/static/private";
+import { createClient } from "@supabase/supabase-js";
+import { SUPABASE_URL, SUPABASE_KEY } from "$env/static/private";
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
-const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY
 
-export const supabase = createClient(supabaseUrl, supabaseKey);
-
+export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
